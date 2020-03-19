24d75cd053cec23d0ec3d6fd7bfdbed593ebf27e
abdulrahman <abd.elsayed@hotmail.com>
2020-03-19 13:01:50 +0400

    component part 2

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,33 +1,81 @@
 import React, { Component } from 'react'
-import { Media } from 'react-bootstrap'
+import { Card } from 'react-bootstrap'
 
 class Menu extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            selectedDish: null
+        }
+
+        this.onCardClick = this.onCardClick.bind(this)
+        this.onDishSelect = this.onDishSelect.bind(this)
+        this.renderDish = this.renderDish.bind(this)
+    }
+    onDishSelect(dish) {
+        this.setState({
+            selectedDish: dish
+        })
+    }
+
+    onCardClick(evt) {
+        let dishId = parseInt(evt.target.id)
+        let selectedDish = this.props.dishes.find((dish) => {
+            return dish.id === dishId
+        })
+
+        this.onDishSelect(selectedDish)
+    }
+
+    renderDish(dish) {
+        if (dish != null) {
+            return (
+                <Card>
+                    <Card.Img className= "img-fluid" src={dish.image} alt={dish.name} />
+                    <Card.Body>
+                        <Card.Title>
+                            {dish.title}
+                        </Card.Title>
+                        <Card.Text>
+                            {dish.description}
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            )
+        }
+
+        else {
+            return (
+                <div></div>
+            )
+        }
+    }
 
     render() {
         const menu = this.props.dishes.map((dish) => {
             return (
-                <Media className = "text-left my-3 p-4 dishWrapper" as="li" key={dish.id}>
-                    <img
+                <div id={dish.id} onClick={this.onCardClick} key={dish.id} className="  m-3 cardWrapper col-12 col-md-5"  >
+                    <Card className="bg-dark"  >
+                        <Card.Img className = "img-fluid" src={dish.image} alt={dish.name} />
+                        <Card.ImgOverlay>
+                            <Card.Title className="text-left" >{dish.name}</Card.Title>
+                            {/* <Card.Text>
+                                {dish.description}
+                            </Card.Text> */}
+                        </Card.ImgOverlay>
+                    </Card>
+                </div>
 
-                        className="mr-3"
-                        src={dish.image}
-                        alt={dish.name}
-                    />
-                    <Media.Body>
-                        <h5>{dish.name}</h5>
-                        <p>
-                            {dish.description}
-                        </p>
-                    </Media.Body>
-                </Media>
             )
         })
         return (
-            <div className="container">
+            <div className="container" >
                 <div className="row">
-                    <ul className="list-unstyled">
-                        {menu}
-                    </ul>
+                    {menu}
+                </div>
+                <div className="row">
+                    {this.renderDish(this.state.selectedDish)}
                 </div>
             </div>
         )
