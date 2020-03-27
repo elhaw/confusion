3821d5938a3bfa16f54eb565bb1e4296977f608b
abdulrahman <abd.elsayed@hotmail.com>
2020-03-27 22:29:33 +0400

    add MainComponent to hold all app state

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
 import React, { Component } from 'react';
-import './App.css';
-import { Navbar, NavbarBrand } from 'react-bootstrap'
-import {DISHES} from './shared/dishes'
-import Menu from './components/MenuComponent'
-
+import MainComponent from './components/MainComponent'
 class App extends Component {
-  constructor(props) {
-    super(props)
 
-    this.state = {
-      dishes: DISHES
-    }
-  }
 
   render() {
     return (
-      <div className="App">
-        <Navbar bg="primary" variant="dark" >
-          <div className="container">
-            <NavbarBrand href="/">
-              Ristorante con fusion
-                </NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes = {this.state.dishes} />
-      </div>
+        <MainComponent /> 
     );
   }
 }
