48337141d37d5a61d9b2cd5fa087343f057817a6
abdulrahman <abd.elsayed@hotmail.com>
2020-03-31 01:21:23 +0400

    move state to main component

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,15 +1,62 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import '../App.css';
-import { Navbar, NavbarBrand } from 'react-bootstrap'
-import {DISHES} from '../shared/dishes'
+import { Navbar, NavbarBrand, Card } from 'react-bootstrap'
+import { DISHES } from '../shared/dishes'
 import Menu from './MenuComponent'
+import DishDetails from './DishdetailsComponent'
+// import DishDetail from './DishdetailComponent';
 
 class MainComponent extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      dishes: DISHES
+      dishes: DISHES,
+      selectedDish: null
+    }
+
+    this.onCardClick = this.onCardClick.bind(this)
+  }
+
+
+  onCardClick(evt) {
+    let dishId = parseInt(evt.target.id)
+    let selectedDish = this.state.dishes.find((dish) => {
+      return dish.id === dishId
+    })
+    this.setState({
+      selectedDish: selectedDish
+    })
+  }
+
+  RenderComments(dish) {
+
+    dish = this.state.selectedDish;
+    if (dish != null) {
+
+      let getdishComments = dish.comments.map((comment) => {
+        return (
+          <li key={comment.id}>
+            <p>{comment.comment}</p>
+            <p><span>{`-- ${comment.author}, `}</span>
+              {new Date(comment.date).toDateString()}
+            </p>
+          </li>
+        )
+      })
+      return (
+        <Card className="cardWrapper col-12 col-md-6 text-left p-3 commentsSection">
+          <ul className="list-unstyled">
+            <h4>Comments</h4>
+            {getdishComments}
+          </ul>
+        </Card>
+      )
+    }
+    else {
+      return (
+        <Fragment></Fragment>
+      )
     }
   }
 
@@ -21,9 +68,10 @@ class MainComponent extends Component {
             <NavbarBrand href="/">
               Ristorante con fusion
                 </NavbarBrand>
-          </div>
+        </div>
         </Navbar>
-        <Menu dishes = {this.state.dishes} />
+        <Menu dishes={this.state.dishes} onCardClick={this.onCardClick} />
+        <DishDetails selectedDish={this.state.selectedDish} />
       </div>
     );
   }
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,37 +1,21 @@
 import React, { Component } from 'react'
 import { Card } from 'react-bootstrap'
-import DishDetail from './DishdetailComponent'
 
 class Menu extends Component {
     constructor(props) {
         super(props)
 
-        this.state = {
-            selectedDish: null
-        }
-
-        this.onCardClick = this.onCardClick.bind(this)
-        this.onDishSelect = this.onDishSelect.bind(this)
+        this.onCardClickEvt = this.onCardClickEvt.bind(this)
     }
 
-    onDishSelect(dish) {
-        this.setState({
-            selectedDish: dish
-        })
+    onCardClickEvt(evt) {
+        this.props.onCardClick(evt)
     }
 
-    onCardClick(evt) {
-        let dishId = parseInt(evt.target.id)
-        let selectedDish = this.props.dishes.find((dish) => {
-            return dish.id === dishId
-        })
-
-        this.onDishSelect(selectedDish)
-    }
     render() {
         const menu = this.props.dishes.map((dish) => {
             return (
-                <div id={dish.id} onClick={this.onCardClick} key={dish.id} className=" mb-4 justify-content-around cardWrapper col-12 col-md-6"  >
+                <div id={dish.id} onClick={this.onCardClickEvt} key={dish.id} className=" mb-4 justify-content-around cardWrapper col-12 col-md-6"  >
                     <Card className="bg-dark"  >
                         <Card.Img className="img-fluid" src={dish.image} alt={dish.name} />
                         <Card.ImgOverlay>
@@ -46,9 +30,6 @@ class Menu extends Component {
             <div className="container" >
                 <div className="row">
                     {menu}
-                </div>
-                <div className="row">
-                       <DishDetail dish  = {this.state.selectedDish} />
                 </div>
             </div>
         )
