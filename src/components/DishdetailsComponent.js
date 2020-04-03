61849e78b3c4c62bf2c480b6c40612febdc99783
abdulrahman <abd.elsayed@hotmail.com>
2020-04-04 01:07:05 +0400

    convert dishdetails component to functional

--- src/components/DishdetailsComponent.js
+++ src/components/DishdetailsComponent.js
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react'
 import { Card } from 'react-bootstrap'
 
 
-function DishDetails(props) {
+function RenderDish(props) {
 
 
     let dish = props.selectedDish;
@@ -48,5 +48,49 @@ function DishDetails(props) {
             <Fragment></Fragment>
         )
     }
+}
+
+function RenderComments(props) {
+    let dish = props.selectedDish;
+    if (dish != null) {
+        let getdishComments = dish.comments.map((comment) => {
+            return (
+                <li key={comment.id}>
+                    <p>{comment.comment}</p>
+                    <p><span>{`-- ${comment.author}, `}</span>
+                        {new Date(comment.date).toDateString()}
+                    </p>
+                </li>
+            )
+        })
+        return (
+            <div className="container">
+                <div className="row">
+                    <Card className="cardWrapper col-12 col-md-6 text-left p-3 commentsSection">
+                        <ul className="list-unstyled">
+                            <h4>Comments</h4>
+                            {getdishComments}
+                        </ul>
+                    </Card>
+                </div>
+            </div>
+        )
+    }
+    else {
+        return (
+            <Fragment></Fragment>
+        )
+    }
+}
+
+function DishDetails({ selectedDish }) {
+    return (
+        <Fragment>
+            <RenderDish dish={selectedDish} />
+            <RenderComments dish={selectedDish} />
+        </Fragment>
+
+    )
+
 }
 export default DishDetails;
