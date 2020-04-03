fbc5222802cf915a8780db59fe5a889e5135503b
abdulrahman <abd.elsayed@hotmail.com>
2020-04-03 14:49:45 +0400

    convert Menu componet to functional component

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,39 +1,35 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Card } from 'react-bootstrap'
 
-class Menu extends Component {
-    constructor(props) {
-        super(props)
 
-        this.onCardClickEvt = this.onCardClickEvt.bind(this)
+
+function Menu(props) {
+
+
+    function onCardClickEvt(evt) {
+        props.onCardClick(evt);
     }
 
-    onCardClickEvt(evt) {
-        this.props.onCardClick(evt)
-    }
-
-    render() {
-        const menu = this.props.dishes.map((dish) => {
-            return (
-                <div id={dish.id} onClick={this.onCardClickEvt} key={dish.id} className=" mb-4 justify-content-around cardWrapper col-12 col-md-6"  >
-                    <Card className="bg-dark"  >
-                        <Card.Img className="img-fluid" src={dish.image} alt={dish.name} />
-                        <Card.ImgOverlay>
-                            <Card.Title className="text-left" >{dish.name}</Card.Title>
-                        </Card.ImgOverlay>
-                    </Card>
-                </div>
-
-            )
-        })
+    const menu = props.dishes.map((dish) => {
         return (
-            <div className="container" >
-                <div className="row">
-                    {menu}
-                </div>
+            <div id={dish.id} onClick={onCardClickEvt} key={dish.id} className=" mb-4 justify-content-around cardWrapper col-12 col-md-6"  >
+                <Card className="bg-dark"  >
+                    <Card.Img className="img-fluid" src={dish.image} alt={dish.name} />
+                    <Card.ImgOverlay>
+                        <Card.Title className="text-left" >{dish.name}</Card.Title>
+                    </Card.ImgOverlay>
+                </Card>
             </div>
+
         )
-    }
+    })
+    return (
+        <div className="container" >
+            <div className="row">
+                {menu}
+            </div>
+        </div>
+    )
 }
 
 
