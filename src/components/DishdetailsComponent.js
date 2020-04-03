41051b1c67963a53da761881f7e38f27ae40c766
abdulrahman <abd.elsayed@hotmail.com>
2020-04-03 14:39:26 +0400

    converted DishDetails comp to functonal component

--- src/components/DishdetailsComponent.js
+++ src/components/DishdetailsComponent.js
@@ -1,52 +1,52 @@
-import React, { Component, Fragment } from 'react'
+import React, { Fragment } from 'react'
 import { Card } from 'react-bootstrap'
-class DishDetails extends Component {
 
 
-    render() {
-        let dish = this.props.selectedDish;
-        if (dish != null) {
-            let getdishComments = dish.comments.map((comment) => {
-                return (
-                    <li key={comment.id}>
-                        <p>{comment.comment}</p>
-                        <p><span>{`-- ${comment.author}, `}</span>
-                            {new Date(comment.date).toDateString()}
-                        </p>
-                    </li>
-                )
-            })
+function DishDetails(props) {
+
+
+    let dish = props.selectedDish;
+    if (dish != null) {
+        let getdishComments = dish.comments.map((comment) => {
             return (
-                <div className="container">
-                    <div className="row">
-                        <div className="cardWrapper col-12 col-md-6 text-left">
-                            <Card className="bg-dark text-light" >
-                                <Card.Img src={dish.image} alt={dish.name} />
-                                <Card.Body>
-                                    <Card.Title>
-                                        {dish.name}
-                                    </Card.Title>
-                                    <Card.Text>
-                                        {dish.description}
-                                    </Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </div>
-                        <Card className="cardWrapper col-12 col-md-6 text-left p-3 commentsSection">
-                            <ul className="list-unstyled">
-                                <h4>Comments</h4>
-                                {getdishComments}
-                            </ul>
+                <li key={comment.id}>
+                    <p>{comment.comment}</p>
+                    <p><span>{`-- ${comment.author}, `}</span>
+                        {new Date(comment.date).toDateString()}
+                    </p>
+                </li>
+            )
+        })
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="cardWrapper col-12 col-md-6 text-left">
+                        <Card className="bg-dark text-light" >
+                            <Card.Img src={dish.image} alt={dish.name} />
+                            <Card.Body>
+                                <Card.Title>
+                                    {dish.name}
+                                </Card.Title>
+                                <Card.Text>
+                                    {dish.description}
+                                </Card.Text>
+                            </Card.Body>
                         </Card>
                     </div>
+                    <Card className="cardWrapper col-12 col-md-6 text-left p-3 commentsSection">
+                        <ul className="list-unstyled">
+                            <h4>Comments</h4>
+                            {getdishComments}
+                        </ul>
+                    </Card>
                 </div>
-            )
-        }
-        else {
-            return (
-                <Fragment></Fragment>
-            )
-        }
+            </div>
+        )
+    }
+    else {
+        return (
+            <Fragment></Fragment>
+        )
     }
 }
 export default DishDetails;
